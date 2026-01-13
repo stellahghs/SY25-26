40ccec0bd308a84b2b3f1c98a7e576a2be8ca73d
stella <sjhug375@learner.dvusd.org>
2026-01-13 13:04:17 -0700

    stage manager update
    i had to take lines 1-8 and copy and paste it from my original pyfest project because i could not push the project properly and mr clarke helped me, and then today i added the lineup and tried to create the loop by printing it however i have not added the more complex parts that take user intel

---
+++ pyfest/pyfest/pyfest.py
@@ -0,0 +1,28 @@
+print("Welcome to Py-Fest 2026 Stage Manager!")
+print("\n---Py-Fest 2026 Stage Manager---")
+print("1. View Lineup & Total Time")
+print("2. Add a New Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move Band to Specific Position")
+choice = input("Select an option 1-6: ")
+
+print("\n--- Current Lineup ---")
+print("1. Code Play (Indie) -- 30 mins")
+print("2. The Pythonistas (Rock) -- 45 mins")
+print("3. Syntax Error (Metal) -- 60 mins")
+print("Total Festival Duration: 135 min")
+
+print("Welcome to Py-Fest 2026 Stage Manager!")
+print("\n---Py-Fest 2026 Stage Manager---")
+print("1. View Lineup & Total Time")
+print("2. Add a New Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move Band to Specific Position")
+choice = input("Select an option 1-6: ")
+
+
+("Code Play", "Indie", 30), ("The Pythonistas", "Rock", 45), ("Syntax Error", "Metal", 60)
+
+
